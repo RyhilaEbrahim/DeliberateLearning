d110f6b1a66139b5888426791bb359c18d3ff32d
Ryhila Ebrahim <ryhila15@gmail.com>
2017-07-14 15:40:24 +0200

    Initial commit

---
+++ DeliberateLearning.Test/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace DeliberateLearning.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void True_GivenTrue_ShouldBeTrue()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(true, true);
+        }
+    }
+}
